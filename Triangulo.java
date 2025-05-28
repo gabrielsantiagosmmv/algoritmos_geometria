54c250b7219b79284c5ee49c07ab83c62a44ddc6
gabrielsantiagosmmv <gabsantestudos@gmail.com>
2025-05-28 20:48:06 -0300

    v2

--- Triangulo.java
+++ Triangulo.java
@@ -1,1 +1,17 @@
-public class Main { public static void main(String[] args) { System.out.println("Ol , GitHub!"); } } 
+import java.util.Scanner;
+public class Triangulo {
+    public static void main(String[] args) { 
+    int base, altura, area;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite o valor da base:");
+    base = scanner.nextInt();
+    System.out.println("Digite o valor da altura:");
+    altura = scanner.nextInt();
+
+    area = base * altura / 2;
+
+    System.out.println("O valor da area do triangulo e: " + area);
+
+    }
+}
